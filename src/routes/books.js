6d117f4000dc6d5c1c8c0af17a484779fbe2e495
curtis-wils0n <curtis.wils0n@protonmail.com>
2022-11-27 14:35:53 -0700

    Create put queries for books, including author, genre, and book. TODO: publishers

--- src/routes/books.js
+++ src/routes/books.js
@@ -26,5 +26,51 @@ module.exports = db => {
     });
   });
 
+  router.put("/books", (request, response) => {
+    const { 
+      author_name,
+      genre,
+      book_title,
+      book_year,
+      book_cover_art_url,
+      pub_name,
+      pub_location
+    } = request.body;
+    console.log(author_name, genre, book_title, book_year, book_cover_art_url, pub_name, pub_location);
+    
+    db.query(
+      `
+      INSERT INTO authors (name)
+      SELECT $1::text
+      WHERE NOT EXISTS (
+        SELECT 1 FROM authors WHERE name ILIKE $1::text
+      );
+      `,
+      [author_name]
+    ).then(() => {
+      db.query(
+        `
+          INSERT INTO genres (name)
+          SELECT $1::text
+          WHERE NOT EXISTS (
+            SELECT 1 FROM genres WHERE name ILIKE $1::text
+          )
+        `,
+        [genre]
+      ).then(() => {
+        db.query(
+          `
+            INSERT INTO books (title, year, cover_art_url, genre_id)
+            SELECT $1::text, $2::integer, $3::text, (SELECT id FROM genres WHERE name ILIKE $4)::integer
+            WHERE NOT EXISTS (
+              SELECT 1 FROM books WHERE title = $1::text
+            );
+          `,
+          [book_title, book_year, book_cover_art_url, genre]
+        )
+      })
+    })
+  });
+
   return router;
 };
