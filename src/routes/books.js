1577cb1c9406895b9a1eecefa96ab83b68c0a06b
curtis-wils0n <curtis.wils0n@protonmail.com>
2022-12-04 13:04:01 -0700

    Create routes for single book view

--- src/routes/books.js
+++ src/routes/books.js
@@ -23,6 +23,28 @@ module.exports = db => {
       });
   });
 
+  router.get("/books/:id", (request, response) => {
+    db.query(`
+      SELECT 
+        books.id,
+        books.title,
+        authors.name AS author_name,
+        books.year,
+        books.cover_art_url,
+        genres.name AS genre,
+        publishers.name AS publisher_name,
+        publishers.location
+      FROM publishers
+      JOIN authors ON publishers.author_id = authors.id
+      JOIN books ON publishers.book_id = books.id
+      JOIN genres ON books.genre_id = genres.id
+      WHERE books.id = $1
+    `, [request.params.id])
+      .then(({ rows: books }) => {
+        response.json(books);
+      });
+  });
+
   router.put("/books", (request, response) => {
     const { 
       author_name,
